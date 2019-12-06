c1f3c88058819d9622f0b012cef372bcd86100d5
jasilven <jasilven@gmail.com>
2019-12-06 17:58:41 +0200

    day 2 part 2

--- src/day2.clj
+++ src/day2.clj
@@ -1,39 +1,57 @@
 (ns day2
   (:require [clojure.string :as str]))
 
-(defn parse-input [s]
+(defn parse-intcodes [s]
   (let [intcodes (-> s str/trim-newline (str/split #","))]
     (mapv #(Integer/parseInt %) intcodes)))
 
-(defn execute-op [[ints pc] op-fn]
-  (let [[a b] (mapv #(aget ints (aget ints (+ % pc))) [1 2])
-        c (aget ints (+ pc 3))]
+(defn execute-op [[ints ip] op-fn]
+  (let [[a b] (mapv #(aget ints (aget ints (+ % ip))) [1 2])
+        c (aget ints (+ ip 3))]
     (aset ints c (op-fn a b))
-    [ints (+ pc 4)]))
+    [ints (+ ip 4)]))
 
 (defn execute
-  "Executes given 'intcodes' which is vector of ints starting at position 'pc'.
-  Returns intcodes vector after execution halts (opcode 99 found)."
-  ([intcodes pc] (execute [(int-array intcodes) 0]))
-  ([[ints pc]]
-   (condp = (aget ints pc)
-     1 (recur (execute-op [ints pc] +))
-     2 (recur (execute-op [ints pc] *))
+  "Executes given 'intcodes' which is vector of ints starting at position 'ip'.
+  Returns intcodes vector after execution halts (oipode 99 found)."
+  ([intcodes ip] (execute [(int-array intcodes) 0]))
+  ([[ints ip]]
+   (condp = (aget ints ip)
+     1 (recur (execute-op [ints ip] +))
+     2 (recur (execute-op [ints ip] *))
      99 (into [] ints)
-     (println "Unknown opcode"))))
+     (throw "Unknown intcode"))))
 
-(defn solve1 [input]
-  (execute input 0))
+(defn solve1 [intcodes]
+  (execute intcodes 0))
+
+(defn nouns-verbs []
+  (for [noun (range 0 100)
+        verb (range 0 100)]
+    [noun verb]))
+
+(defn answer2 [intcodes]
+  (+ (* 100 (nth intcodes 1)) (nth intcodes 2)))
+
+(defn solve2 [intcodes wanted]
+  (->> (map #(assoc intcodes
+                    1 (first %)
+                    2 (second %)) (nouns-verbs))
+       (filter #(= wanted (try (first (execute % 0))
+                               (catch Exception e nil))))
+       first
+       answer2))
 
 (defn -main [& args]
-  (let [input (-> (slurp "resources/day2-input.txt")
-                  parse-input
-                  (assoc 1 12 2 2))]
-    (println "part 1:" (first (solve1 input)))
-    (println "part 2:" (first (solve1 input)))))
+  (let [intcodes (-> (slurp "resources/day2-input.txt")
+                     parse-intcodes
+                     (assoc 1 12 2 2))]
+    (println "part 1:" (first (solve1 intcodes)))
+    (println "part 2:" (solve2 intcodes 19690720))))
 
 (comment
-  (-> "resources/day2-input.txt" slurp parse-input (assoc 1 12 2 2) solve1 first)
+  (-> "resources/day2-input.txt" slurp parse-intcodes (assoc 1 12 2 2) solve1 first)
   ;; => 4138687
-
+  (-> "resources/day2-input.txt" slurp parse-intcodes (solve2 19690720))
+  ;; => 6635  
   )
