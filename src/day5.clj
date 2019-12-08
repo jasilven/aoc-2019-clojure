d6765b0ea6bfeda4ea7493d4d8d139cf69f9a4d4
jasilven <jasilven@gmail.com>
2019-12-08 13:32:32 +0200

    day 5 part 2

--- src/day5.clj
+++ src/day5.clj
@@ -3,30 +3,50 @@
             [day4]
             [clojure.string :as str]))
 
+(defn parse-instruction [intcode]
+  (let [digits (day4/int->digits intcode)
+        [a b c d e] (concat (repeat (- 5 (count digits)) 0)
+                            digits)]
+    {:A a :B b :C c :opcode (+ (* d 10) e)}))
+
+(defn get-param [[ints ip] value position]
+  (if (= 0 value) (aget ints (aget ints (+ position ip)))
+      (aget ints (+ position ip))))
+
+(defn parse-params [[ints ip] {:keys [B C]} n]
+  (let [arg1 (get-param [ints ip] C 1)
+        arg2 (get-param [ints ip] B 2)
+        arg3 (when (= n 3) (aget ints (+ 3 ip)))]
+    (take n [arg1 arg2 arg3])))
+
 (defn execute-input [[ints ip]]
   (->> (Integer/parseInt (read-line))
        (aset ints (aget ints (inc ip))))
   [ints (+ 2 ip)])
 
-(defn execute-output [[ints ip]]
-  (println (aget ints (aget ints (inc ip))))
+(defn execute-output [[ints ip] {:keys [C]}]
+  (println (get-param [ints ip] C 1))
   [ints (+ 2 ip)])
 
-(defn parse-instruction [intcode]
-  (let [digits (day4/int->digits intcode)
-        [a b c d e] (concat (repeat (- 5 (count digits)) 0) digits)]
-    {:A a :B b :C c :opcode (+ (* d 10) e)}))
+(defn execute-op [[ints ip] op-fn {:keys [B C] :as instruction}]
+  (if (= 0 B C)
+    (day2/execute-op [ints ip] op-fn)
+    (do
+      (aset ints
+            (aget ints (+ ip 3))
+            (apply op-fn (parse-params [ints ip] instruction 2)))
+      [ints (+ 4 ip)])))
 
-(defn execute-op [[ints ip] op-fn instruction]
-  (let [b (:B instruction) c (:C instruction)]
-    (if (= 0 b c)
-      (day2/execute-op [ints ip] op-fn)
-      (let [arg1 (if (= 0 c) (aget ints (aget ints (+ 1 ip)))
-                     (aget ints (+ 1 ip)))
-            arg2 (if (= 0 b) (aget ints (aget ints (+ 2 ip)))
-                     (aget ints (+ 2 ip)))]
-        (aset ints (aget ints (+ ip 3)) (op-fn arg1 arg2))
-        [ints (+ 4 ip)]))))
+(defn execute-jump [[ints ip] test-fn instruction]
+  (let [[arg1 arg2] (parse-params [ints ip] instruction 2)]
+    (if (test-fn arg1)
+      [ints arg2]
+      [ints (+ 3 ip)])))
+
+(defn execute-cmp [[ints ip] cmp-fn instruction]
+  (let [[arg1 arg2 arg3] (parse-params [ints ip] instruction 3)]
+    (aset ints arg3 (if (cmp-fn arg1 arg2) 1 0))
+    [ints (+ ip 4)]))
 
 (defn execute
   ([intcodes ip] (execute [(int-array intcodes) 0]))
@@ -36,12 +56,16 @@
        1 (recur (execute-op [ints ip] + instruction))
        2 (recur (execute-op [ints ip] * instruction))
        3 (recur (execute-input [ints ip]))
-       4 (recur (execute-output [ints ip]))
+       4 (recur (execute-output [ints ip] instruction))
+       5 (recur (execute-jump [ints ip] (comp not zero?) instruction))
+       6 (recur (execute-jump [ints ip] zero? instruction))
+       7 (recur (execute-cmp [ints ip] < instruction))
+       8 (recur (execute-cmp [ints ip] = instruction))
        (day2/execute [ints ip])))))
 
-(defn solve1 [intcodes]
+(defn solve [intcodes input]
   (-> (with-out-str
-        (with-in-str "1"
+        (with-in-str input
           (execute intcodes 0)))
       (str/split-lines)
       last))
@@ -49,9 +73,14 @@
 (defn -main [& args]
   (let [intcodes (-> (slurp "resources/day5-input.txt")
                      day2/parse-intcodes)]
-    (println "part 1:" (solve1 intcodes))))
+    (println "part 1:" (solve intcodes "1"))
+    (println "part 2:" (solve intcodes "5"))))
 
 (comment
-  (-> "resources/day5-input.txt" slurp day2/parse-intcodes solve1)
+  (-> "resources/day5-input.txt" slurp day2/parse-intcodes
+      (solve "1"))
   ;; => "16489636"
+  (-> "resources/day5-input.txt" slurp day2/parse-intcodes
+      (solve "5"))
+  ;; => "9386583"
   )
--- test/day5_test.clj
+++ test/day5_test.clj
@@ -1,5 +1,6 @@
 (ns day5-test
   (:require [day5 :as sut]
+            [day2]
             [clojure.test :as t]))
 
 
@@ -7,3 +8,61 @@
   (t/is (= "77\n" (with-in-str "77"
                     (with-out-str
                       (sut/execute [3 0 4 0 99] 0))))))
+
+(t/deftest day5-part2 
+  (t/is (= "1\n" 
+           (with-out-str
+             (-> "3,9,8,9,10,9,4,9,99,-1,8" day2/parse-intcodes
+                 (sut/solve2 "8")))))
+  (t/is (= "0\n" 
+           (with-out-str
+             (-> "3,9,8,9,10,9,4,9,99,-1,8" day2/parse-intcodes
+                 (sut/solve2 "9")))))
+  (t/is (= "1\n" 
+           (with-out-str
+             (-> "3,9,7,9,10,9,4,9,99,-1,8" day2/parse-intcodes
+                 (sut/solve2 "7")))))
+  (t/is (= "0\n" 
+           (with-out-str
+             (-> "3,9,7,9,10,9,4,9,99,-1,8" day2/parse-intcodes
+                 (sut/solve2 "9")))))
+  (t/is (= "1\n" 
+           (with-out-str
+             (-> "3,3,1108,-1,8,3,4,3,99" day2/parse-intcodes
+                 (sut/solve2 "8")))))
+  (t/is (= "0\n" 
+           (with-out-str
+             (-> "3,3,1108,-1,8,3,4,3,99" day2/parse-intcodes
+                 (sut/solve2 "0")))))
+  (t/is (= "1\n" 
+           (with-out-str
+             (-> "3,3,1107,-1,8,3,4,3,99" day2/parse-intcodes
+                 (sut/solve2 "0")))))
+  (t/is (= "0\n" 
+           (with-out-str
+             (-> "3,3,1107,-1,8,3,4,3,99" day2/parse-intcodes
+                 (sut/solve2 "9"))))))
+
+(t/deftest day5-part2-jump
+  (t/is (= "0\n" 
+           (with-out-str
+             (-> "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9" day2/parse-intcodes
+                 (sut/solve2 "0")))))
+  (t/is (= "1\n" 
+           (with-out-str
+             (-> "3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9" day2/parse-intcodes
+                 (sut/solve2 "1"))))))
+
+(t/deftest day5-part2-larger-example
+  (t/is (= "999\n" 
+           (with-out-str
+             (-> "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" day2/parse-intcodes
+                 (sut/solve2 "1")))))
+  (t/is (= "1000\n" 
+           (with-out-str
+             (-> "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" day2/parse-intcodes
+                 (sut/solve2 "8")))))
+  (t/is (= "1001\n" 
+           (with-out-str
+             (-> "3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99" day2/parse-intcodes
+                 (sut/solve2 "900"))))))
